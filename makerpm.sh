9865eaff517a2ffb37cd2c86d399fd2777d3807c
Jake O'Shannessy <joshannessy@gmail.com>
2021-12-20 23:38:32 +0000

    Copy patches to correct locatoin

--- makerpm.sh
+++ makerpm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export QA_RPATHS=7
-rpmdev-setuptree
-spectool -g -R fds.spec
-rpmbuild -ba fds.spec
+# rpmdev-setuptree
+mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+spectool -g fds.spec -C rpmbuild/SOURCES --all
+rpmbuild -ba fds.spec --define "_topdir $(pwd)/rpmbuild"
