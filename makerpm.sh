8c4c7cc86aa921029859b6b742c5b8a14cc91a42
Jake O'Shannessy <joshannessy@gmail.com>
2021-12-21 15:19:35 +0000

    Correct tests

--- makerpm.sh
+++ makerpm.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export QA_RPATHS=7
-# rpmdev-setuptree
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 spectool -g fds.spec -C rpmbuild/SOURCES --all
 rpmbuild -ba fds.spec --define "_topdir $(pwd)/rpmbuild"
--- run-tests.sh
+++ run-tests.sh
@@ -10,29 +10,27 @@ trap 'err_report $LINENO' ERR
 export OMP_NUM_THREADS=1
 
 test_fds() {
-    readarray -d + -t strarr <<< "$1"
-    VERSION=${strarr[0]}
-    echo $VERSION
-    mkdir -p "test/$1" && cp "test-inputs/$VERSION.fds" "test/$1/input.fds" && pushd "test/$1" && "/opt/FDS/$1/bin/fds" 2 input.fds && popd
+    mkdir -p "test/$1" && cp "test-inputs/$1.fds" "test/$1/input.fds" && pushd "test/$1" && "fds-$1" 2 input.fds && popd
 }
 
-test_fds 6.1.2+smokecloud.ifort
-test_fds 6.2.0+smokecloud.ifort
-test_fds 6.3.0+smokecloud.ifort
-test_fds 6.3.1+smokecloud.ifort
-test_fds 6.3.2+smokecloud.ifort
-test_fds 6.4.0+smokecloud.ifort
-test_fds 6.5.0+smokecloud.ifort
-test_fds 6.5.1+smokecloud.ifort
-test_fds 6.5.2+smokecloud.ifort
-test_fds 6.5.3+smokecloud.ifort
-test_fds 6.6.0+smokecloud.ifort
-test_fds 6.7.0+smokecloud.ifort
-test_fds 6.7.1+smokecloud.ifort
-test_fds 6.7.3+smokecloud.ifort
-test_fds 6.7.4+smokecloud.ifort
-test_fds 6.7.5+smokecloud.ifort
-test_fds 6.7.6+smokecloud.ifort
-test_fds 6.7.7+smokecloud.ifort
+test_fds 5.5.3
+test_fds 6.1.2
+test_fds 6.2.0
+test_fds 6.3.0
+test_fds 6.3.1
+test_fds 6.3.2
+test_fds 6.4.0
+test_fds 6.5.0
+test_fds 6.5.1
+test_fds 6.5.2
+test_fds 6.5.3
+test_fds 6.6.0
+test_fds 6.7.0
+test_fds 6.7.1
+test_fds 6.7.3
+test_fds 6.7.4
+test_fds 6.7.5
+test_fds 6.7.6
+test_fds 6.7.7
 
 echo "tests run successfully"
