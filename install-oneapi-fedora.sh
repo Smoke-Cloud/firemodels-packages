d5399bd212beef8ccff2fcdb560f014619138053
Jake O'Shannessy <joshannessy@gmail.com>
2021-12-30 14:16:56 +0000

    CI: Install intel compilers

--- install-oneapi-fedora.sh
+++ install-oneapi-fedora.sh
@@ -9,6 +9,6 @@ repo_gpgcheck=1
 gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 EOF
 
-sudo mv /tmp/oneAPI.repo /etc/yum.repos.d
+mv /tmp/oneAPI.repo /etc/yum.repos.d
 
-sudo dnf install intel-basekit intel-hpckit
+dnf install intel-basekit intel-hpckit
