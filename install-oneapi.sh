e678570ea5a41486a3b94cdd2f47d86107000a9e
Jake O'Shannessy <joshannessy@smokecloud.io>
2021-12-31 03:14:30 +0000

    Arch: Fix 6.4.0 hashes

--- install-oneapi.sh
+++ install-oneapi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18236/l_BaseKit_p_2021.4.0.3422_offline.sh
+wget -nv https://registrationcenter-download.intel.com/akdlm/irc_nas/18236/l_BaseKit_p_2021.4.0.3422_offline.sh
 chmod 755 l_BaseKit_p_2021.4.0.3422_offline.sh
-wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18211/l_HPCKit_p_2021.4.0.3347_offline.sh
+wget -nv https://registrationcenter-download.intel.com/akdlm/irc_nas/18211/l_HPCKit_p_2021.4.0.3347_offline.sh
 chmod 755 l_HPCKit_p_2021.4.0.3347_offline.sh
 
 ./l_BaseKit_p_2021.4.0.3422_offline.sh -s -a --silent  --eula accept --intel-sw-improvement-program-consent decline --components intel.oneapi.lin.ippcp.devel:intel.oneapi.lin.tbb.devel:intel.oneapi.lin.dpcpp_dbg:intel.oneapi.lin.dpcpp-ct:intel.oneapi.lin.dpcpp-cpp-compiler:intel.oneapi.lin.dnnl:intel.oneapi.lin.mkl.devel:intel.oneapi.lin.ipp.devel:intel.oneapi.lin.python3:intel.oneapi.lin.ccl.devel:intel.oneapi.lin.dpl:intel.oneapi.lin.vtune
