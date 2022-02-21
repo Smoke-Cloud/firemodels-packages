294c9b464783e2efb64f8b9ca5bf653b7a84b9cd
Jake O'Shannessy <joshannessy@gmail.com>
2022-02-21 22:19:47 +0000

    Fix version numbers

--- 6.5.0/deb/build_debian.sh
+++ 6.5.0/deb/build_debian.sh
@@ -4,8 +4,8 @@ version=6.5.0
 wget https://github.com/firemodels/${repo}/archive/${commit}.zip
 unzip ${commit}.zip
 pushd ${repo}-${commit}
-tar -czvf ../fds_${version}.orig.tar.gz .
+tar -czvf ../fds-${version}_${version}.orig.tar.gz .
 popd
 pushd fds-${version}
-tar -xzvf ../fds_${version}.orig.tar.gz
+tar -xzvf ../fds-${version}_${version}.orig.tar.gz
 debuild --no-sign
