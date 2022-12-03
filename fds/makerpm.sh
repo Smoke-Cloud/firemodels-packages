a72355a67fbdab3df91b2963777a9f9b25c5c4d4
Jake O'Shannessy <joshannessy@gmail.com>
2022-12-03 12:35:42 +0000

    FDS: Fix a build script path

--- fds/makerpm.sh
+++ fds/makerpm.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 export QA_RPATHS=7
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-zip rpmbuild/SOURCES/fds.sh.zip ../fds.sh
+zip rpmbuild/SOURCES/fds.sh.zip ../../fds.sh
 cp backports.patch rpmbuild/SOURCES || true
 cp version.patch rpmbuild/SOURCES || true
 cat head.spec ../../template.spec > fds.spec
