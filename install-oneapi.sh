11df55bb081e5327de846da2d0675fc2ebc0cfd4
Jake O'Shannessy <joshannessy@gmail.com>
2022-12-03 12:07:12 +0000

    Remove Arch Linux support

--- install-oneapi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget -nv https://registrationcenter-download.intel.com/akdlm/irc_nas/18236/l_BaseKit_p_2021.4.0.3422_offline.sh
-chmod 755 l_BaseKit_p_2021.4.0.3422_offline.sh
-wget -nv https://registrationcenter-download.intel.com/akdlm/irc_nas/18211/l_HPCKit_p_2021.4.0.3347_offline.sh
-chmod 755 l_HPCKit_p_2021.4.0.3347_offline.sh
-
-./l_BaseKit_p_2021.4.0.3422_offline.sh -s -a --silent  --eula accept --intel-sw-improvement-program-consent decline --components intel.oneapi.lin.ippcp.devel:intel.oneapi.lin.dpcpp-cpp-compiler:intel.oneapi.lin.mkl.devel
-./l_HPCKit_p_2021.4.0.3347_offline.sh   -s -a --silent  --eula accept --intel-sw-improvement-program-consent decline --components intel.oneapi.lin.inspector:intel.oneapi.lin.mpi.devel:intel.oneapi.lin.itac:intel.oneapi.lin.clck:intel.oneapi.lin.ifort-compiler:intel.oneapi.lin.dpcpp-cpp-compiler-pro
