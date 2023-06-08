be76e0621be9c4b983dab3e0e30ae04165696fe6
Jake O'Shannessy <joshannessy@gmail.com>
2023-06-08 10:29:07 +0100

    SMV: Add RPMs to dist

--- smv/buildrpm.sh
+++ smv/buildrpm.sh
@@ -7,3 +7,5 @@ cp ${spec_path} rpmbuild/SPECS/
 git archive --output=rpmbuild/SOURCES/${src_name}-"${version}".tar.gz --prefix=${src_name}-"${version}"/ HEAD
 spectool -g rpmbuild/SPECS/${spec_path} -C rpmbuild/SOURCES --all
 rpmbuild -ba rpmbuild/SPECS/${spec_path} --define "_topdir $(pwd)/rpmbuild"
+mkdir -p ../dist
+cp rpmbuild/RPMS/"$(rpmbuild --eval '%{_arch}')"/*.rpm ../dist/
