7f641d0d907c4dd6ba0465bf7fd48ab57c025121
Jake O'Shannessy <joshannessy@gmail.com>
2025-01-02 07:58:25 +0000

    simplify build script

--- fds/buildrpm.sh
+++ fds/buildrpm.sh
@@ -40,34 +40,6 @@ rpmbuild -ba fds.spec \
     --define "revision_date ${revision_date}" \
     --define "version_patch ${version_patch}" \
     --define "backports_patch ${backports_patch}" \
-     "$@"
+     "$@"  --noclean
 mkdir -p ../../../dist
 cp rpmbuild/RPMS/"$(rpmbuild --eval '%{_arch}')"/*.rpm ../../../dist/
-
-if [ "$version" = "$latest" ]; then
-    mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-    rm rpmbuild/SOURCES/fds.sh.zip || true
-    zip rpmbuild/SOURCES/fds.sh.zip ../../fds.sh
-    cp ../../"$backports_patch" rpmbuild/SOURCES || true
-    cp ../../"$version_patch"  rpmbuild/SOURCES || true
-    cat ../../"$version".spec ../../template.spec > fds.spec
-    spectool -g fds.spec -C rpmbuild/SOURCES --all \
-        --define "_topdir $(pwd)/rpmbuild" \
-        --define "this_version ${version}" \
-        --define "repo ${repo}" \
-        --define "commit ${commit}" \
-        --define "revision_date ${revision_date}" \
-        --define "version_patch ${version_patch}" \
-        --define "backports_patch ${backports_patch}"
-    rpmbuild -ba fds.spec \
-        --define "_topdir $(pwd)/rpmbuild" \
-        --define "this_version ${version}" \
-        --define "repo ${repo}" \
-        --define "commit ${commit}" \
-        --define "revision_date ${revision_date}" \
-        --define "version_patch ${version_patch}" \
-        --define "backports_patch ${backports_patch}" \
-        "$@" --noclean
-    mkdir -p ../../../dist
-    cp rpmbuild/RPMS/"$(rpmbuild --eval '%{_arch}')"/*.rpm ../../../dist/
-fi
